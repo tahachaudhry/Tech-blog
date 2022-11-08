3907dce9a95cd9a0ecd753b125c3e3561947ff67
Taha Chaudhry <taha@nyu.edu>
2022-11-08 17:46:21 -0600

    initial commit

---
+++ config/config.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize('tech_blog_db', 'root', 'MSUbootcamp', {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
+
+
+module.exports = sequelize;

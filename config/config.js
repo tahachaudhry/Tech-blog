3cd9cdb4a5dc2221f4c00a5fa88117131832ddc9
Taha Chaudhry <taha@nyu.edu>
2022-11-08 20:24:32 -0600

    bug fix

--- config/config.js
+++ config/config.js
@@ -7,8 +7,8 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize('tech_blog_db', 'root', '', {
-        host: 'localhost',
+    sequelize = new Sequelize('heroku_048929a63d8b6fd', 'b0bdf3eabc4d81', '52c84867', {
+        host: 'us-cdbr-east-06.cleardb.net',
         dialect: 'mysql',
         port: 3306
     });
@@ -16,3 +16,4 @@ if (process.env.JAWSDB_URL) {
 
 
 module.exports = sequelize;
+
